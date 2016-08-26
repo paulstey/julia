6e6cc4e538737fd54e2c6afcd83c817859a4ecb0
Jameson Nash <vtjnash@gmail.com>
2016-08-26 13:50:50 -0400

    add tests for #265

---
+++ test/worlds.jl
@@ -0,0 +1,13 @@
+# This file is a part of Julia. License is MIT: http://julialang.org/license
+
+# tests for accurate updating of method tables
+
+# test method replacement
+
+# test signature widening
+
+# test signature narrowing
+
+# test constructors
+
+# test oldworld call / inference
